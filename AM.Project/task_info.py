b880560662f4ee8e6d4870d2d7384213ccdd3d16
Zalkin <Zalkin.viktor@mail.ru>
2019-05-12 12:31:11 +0300

    TI. Add second_function(x, y, z) from variant 9

--- AM.Project/task_info.py
+++ AM.Project/task_info.py
@@ -1,4 +1,15 @@
 import math
 
+
+
 def first_function(x):
     return math.e**(1 / (1 + x))
+
+
+def second_function(x, y, z):
+    # (x | y) -> (y -> z)
+    # !(x and y) -> (!y or z)
+    # (x and y) or (!y or z)
+    # x and y or !y or z
+    # x or !y or z
+    return int(x or not y or z)

469e8dec9667eeb17daf98e05585442524057688
Zalkin <Zalkin.viktor@mail.ru>
2019-05-13 21:58:47 +0300

    DG. Add generate_logical()

--- AM.Project/data_generator.py
+++ AM.Project/data_generator.py
@@ -1,1 +1,20 @@
+import numpy as np
 
+
+
+DELIMETER = ","
+
+
+
+def generate_logical(rows_count, function, filename="logical.csv"):
+    random_data = np.random.rand(rows_count, 3)
+    random_logical = []
+
+    for row in random_data:
+        random_logical.append((list(map(lambda x: int(round(x)), row))))
+    
+    with open(filename, "w") as file:
+        file.write(DELIMETER.join(["x", "y", "z", "result"]) + "\n")
+        for row in random_logical:
+            row.append(function(*row))
+            file.write(DELIMETER.join(map(str, row)) + "\n")

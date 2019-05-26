d8c4eafb9abd93aec2eaa4303f32593cd71cb93a
Zalkin <Zalkin.viktor@mail.ru>
2019-05-26 12:13:34 +0300

    NN. Add class NeuralNetwork with constructor

--- AM.Project/AM.Project.py
+++ AM.Project/AM.Project.py
@@ -1,1 +1,6 @@
+from NeuralNetwork import NeuralNetwork
 
+
+
+if __name__ == "__main__":
+    network = NeuralNetwork(3, 2, 1)
--- AM.Project/NeuralNetwork.py
+++ AM.Project/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 
@@ -8,3 +9,10 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+class NeuralNetwork:
+    def __init__(self, input_count, neurons_count, learning_rate):
+        self.learning_rate = np.array(learning_rate)
+        self.weights_input_hidden = np.random.randn(neurons_count, input_count)
+        self.weights_hidden_output = np.random.randn(1, neurons_count)

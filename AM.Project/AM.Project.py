5902472588d69a9e366ed9e3933b74e6db3d22e5
Zalkin <Zalkin.viktor@mail.ru>
2019-05-26 17:52:14 +0300

    AMP. Add epochs, inputs_count, neurons_count, learning_rate

--- AM.Project/AM.Project.py
+++ AM.Project/AM.Project.py
@@ -10,10 +10,16 @@ def MSE(actual, expected):
 
 
 if __name__ == "__main__":
+    epochs = 1000
+
+    inputs_count = 3
+    neurons_count = 2
+    learning_rate = 1
+
     logic_data = np.array(DG.read_csv("logical.csv", int), np.int32)
     regr_raw_data = np.array(DG.read_csv("regression.csv", float), np.float)
 
 
-    network = NeuralNetwork(3, 2, 1)
+    network = NeuralNetwork(inputs_count, neurons_count, learning_rate)
     result = network.predict([0,1,1])
     train = network.train(np.array([0,1,1]), 1)

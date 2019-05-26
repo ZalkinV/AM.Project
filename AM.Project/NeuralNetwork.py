89b7f0c7ac3e3ab4922ff435761be102c9649afe
Zalkin <Zalkin.viktor@mail.ru>
2019-05-26 13:19:41 +0300

    NN. Add self.activation and self.gradient for sigmoid

--- AM.Project/NeuralNetwork.py
+++ AM.Project/NeuralNetwork.py
@@ -16,13 +16,15 @@ class NeuralNetwork:
         self.learning_rate = np.array(learning_rate)
         self.weights_input_hidden = np.random.randn(neurons_count, input_count)
         self.weights_hidden_output = np.random.randn(1, neurons_count)
+        self.activation = np.vectorize(sigmoid)
+        self.gradient = np.vectorize(lambda x_sigmoided: x_sigmoided * (1 - x_sigmoided))
 
 
     def predict(self, input):
         hidden_in = np.dot(self.weights_input_hidden, input)
-        hidden_out = np.array(list(map(sigmoid, hidden_in)))
+        hidden_out = self.activation(hidden_in)
 
         output_in = np.dot(self.weights_hidden_output, hidden_out)
-        output_out = np.array(list(map(sigmoid, output_in)))
+        output_out = self.activation(output_in)
 
         return output_out

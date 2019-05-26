c569a9f4d6c3b051980e6f762a50ac9ddcf8cb8f
Zalkin <Zalkin.viktor@mail.ru>
2019-05-26 17:38:08 +0300

    AMP. Add logic and regression data reading

--- AM.Project/AM.Project.py
+++ AM.Project/AM.Project.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from NeuralNetwork import NeuralNetwork
-
+import data_generator as DG
 
 
 
@@ -10,6 +10,19 @@ def MSE(actual, expected):
 
 
 if __name__ == "__main__":
+    logic_raw_data = DG.read_csv("logical.csv")
+    logic_data = []
+    for row in logic_raw_data:
+        logic_data.append(list(map(int, row)))
+    logic_data = np.array(logic_data)
+
+    regr_raw_data = DG.read_csv("regression.csv")
+    regr_data = []
+    for row in regr_raw_data:
+        regr_data.append(list(map(float, row)))
+    regr_data = np.array(regr_data)
+
+
     network = NeuralNetwork(3, 2, 1)
     result = network.predict([0,1,1])
     train = network.train(np.array([0,1,1]), 1)

2f0afd87566c3ad551e426ccc270eaecfba1036c
Zalkin <Zalkin.viktor@mail.ru>
2019-05-26 17:04:39 +0300

    NN. Implement train()

--- AM.Project/AM.Project.py
+++ AM.Project/AM.Project.py
@@ -1,7 +1,11 @@
+import numpy as np
+
 from NeuralNetwork import NeuralNetwork
+
 
 
 
 if __name__ == "__main__":
     network = NeuralNetwork(3, 2, 1)
     result = network.predict([0,1,1])
+    train = network.train(np.array([0,1,1]), 1)
--- AM.Project/NeuralNetwork.py
+++ AM.Project/NeuralNetwork.py
@@ -13,6 +13,7 @@ def sigmoid_derivative(x):
 
 class NeuralNetwork:
     def __init__(self, input_count, neurons_count, learning_rate):
+        np.random.seed(0)
         self.learning_rate = np.array(learning_rate)
         self.weights_input_hidden = np.random.randn(neurons_count, input_count)
         self.weights_hidden_output = np.random.randn(1, neurons_count)
@@ -28,3 +29,20 @@ class NeuralNetwork:
         output_out = self.activation(output_in)
 
         return output_out
+
+
+    def train(self, input, expected_output):
+        hidden_in = np.dot(self.weights_input_hidden, input)
+        hidden_out = self.activation(hidden_in)
+
+        output_in = np.dot(self.weights_hidden_output, hidden_out)
+        output_out = self.activation(output_in)
+
+
+        error_output = output_out - expected_output
+        weights_hidden_output_delta = error_output * self.gradient(output_out)
+        self.weights_hidden_output -= np.dot(weights_hidden_output_delta, hidden_out.reshape(1, len(hidden_out))) * self.learning_rate
+        
+        error_hidden = self.weights_hidden_output * weights_hidden_output_delta
+        weights_input_hidden_delta = error_hidden * self.gradient(hidden_out)
+        self.weights_input_hidden -= np.dot(input.reshape(len(input), 1), weights_input_hidden_delta).T * self.learning_rate

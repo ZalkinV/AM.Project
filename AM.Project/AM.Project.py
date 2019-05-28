1e971c2a39310f0f87bd65064dd7cbd48daeada8
Zalkin <Zalkin.viktor@mail.ru>
2019-05-28 13:57:36 +0300

    Work for report

--- AM.Project/AM.Project.py
+++ AM.Project/AM.Project.py
@@ -11,7 +11,7 @@ def MSE(actual, expected):
     return np.mean((actual - expected)**2)
 
 
-def train_network(network, data, epochs):
+def train_network(network, data, epochs, is_plot):
     mse_losses = []
     for epoch in range(epochs):
         mse_sum = 0
@@ -24,9 +24,10 @@ def train_network(network, data, epochs):
         print(f"Epoch {epoch + 1}/{epochs}: MSE = {mse_losses[-1]}", end="\r")
     print()
     
-    plt.plot(range(1, epochs + 1), mse_losses)
-    plt.xlabel("Epochs")
-    plt.ylabel("Mean Squared Error")
+    if is_plot:
+        plt.plot(range(1, epochs + 1), mse_losses)
+        plt.xlabel("Epochs")
+        plt.ylabel("Mean Squared Error")
     return mse_losses[-1]
     
 
@@ -46,10 +47,10 @@ def test_net_on_logic_func(net, func, parameters_count):
 
 
 
-def network_work(epochs, layers, data, learning_rate, func, parameters_count):
+def network_work(epochs, layers, data, learning_rate, func, parameters_count, is_plot=True):
     network = NeuralNetwork(layers, learning_rate)
    
-    mse_loss = train_network(network, data, epochs)
+    mse_loss = train_network(network, data, epochs, is_plot)
     print("Results:")
     test_net_on_logic_func(network, func, parameters_count)
     print()
@@ -73,16 +74,29 @@ if __name__ == "__main__":
     plt.show()
 
 
-    print("Neurons count test for first function:")
+    
+    plt.xticks(range(1, 20))
+    plt.xlabel("Neurons count")
+    plt.ylabel("Mean Squared Error")
+
+    print("\n\nNeurons count test for first function:")
     epochs = 50
-    for i in range(1, 20):
+    mse_losses = []
+    max_neuron = 17
+    for i in range(1, max_neuron):
         logic_layers[1] = i
         print(f"Hidden layer neurons count = {i}")
-        network_work(epochs, logic_layers, logic_data, learning_rate, TI.second_function, 3)
-
+        mse_losses.append(round(network_work(epochs, logic_layers, logic_data, learning_rate, TI.second_function, 3, False), 4))
+    plt.plot(range(1, max_neuron), mse_losses)
 
     print("\nNeurons count test for second function:")
-    for i in range(1, 20):
+    mse_losses.clear()
+    for i in range(1, max_neuron):
+        logic_layers[0] = 2
         logic_layers[1] = i
         print(f"Hidden layer neurons count = {i}")
-        network_work(epochs, logic_layers, logic_data, learning_rate, TI.second_function, 3)
+        mse_losses.append(round(network_work(epochs, logic_layers, logic_data_2, learning_rate, TI.third_function, 2, False), 4))
+    plt.plot(range(1, max_neuron), mse_losses)
+
+    plt.legend(["First function", "Second functon"])
+    plt.show()
